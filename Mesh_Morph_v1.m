b33cc85aa8ca1f40567811ab0c840b323186a3f8
thor-andreassen <thor.andreassen@du.edu>
2022-12-20 16:09:34 -0700

    Created separate function for morphing

--- Mesh_Morph_v1.m
+++ Mesh_Morph_v1.m
@@ -3,23 +3,7 @@ clear
 close all
 clc
 
-%% constant parameters
-% f=10000;
-% p=2;
-% f=10;
-% p=2;
 
-
-% f=1000;
-% p=10;
-
-f=10;
-p=2;
-rand_mult=1;
-
-%% Initialize Figures
-fig_mesh=figure();
-fig_error=figure();
 
 %% load target mesh
 stl_path=['C:\Users\Thor.Andreassen\Desktop\Thor Personal Folder\Research\Laxity FE Model\S192803_FE_Model\Iterative Alignment Check\MeshMorph\mesh_geometries\'];
@@ -62,153 +46,181 @@ source.nodes=source_nodes_renumber;
 source.faces=source_tri_elems;
 source.nodes_reduce=source_nodes_renumber;
 source.faces_reduce=source_tri_elems;
-%% plot original bones
-% target_geom_orig=patch('Faces',target.faces,'Vertices',target.nodes,'FaceColor','r','EdgeAlpha',.2);
-% hold on
-% source_geom_orig=patch('Faces',source.faces_reduce,'Vertices',source.nodes_reduce,'FaceColor','b','EdgeAlpha',.2);
 
 
-%% mesh morphin initialization
-target_nodes=target.nodes;
-source_nodes_0=source.nodes_reduce;
-source_nodes=source_nodes_0;
-bb_min=min(source.nodes_reduce);
-bb_max=max(source.nodes_reduce);
-bb_dif=abs(bb_max-bb_min)*.3;
-bb_min=bb_min-bb_dif;
-bb_max=bb_max+bb_dif;
+%% morph large f
+params.f=1000;
+params.new_knots_per_iter=20;
+params.f_decay=.995;
+params.max_iterations=15;
+params.want_plot=1;
+params.rand_mult=2;
+params.rand_decay=.999;
+params.include_rand_knots=1;
+params.knot_reset_iter=10;
+params.target_source_switch_iter=2;
+params.start_target=1;
+params.initial_knots=4;
 
-x_bb=linspace(bb_min(1),bb_max(1),3);
-y_bb=linspace(bb_min(2),bb_max(2),3);
-z_bb=linspace(bb_min(3),bb_max(3),3);
-
-[X,Y,Z]=meshgrid(x_bb,y_bb,z_bb);
-X=reshape(X,[],1);
-Y=reshape(Y,[],1);
-Z=reshape(Z,[],1);
-
-bb_dif=bb_max-bb_min;
-K0=[X,Y,Z];
-
-K=K0;
-% hold on
-% scatter3(K(:,1),K(:,2),K(:,3),'go')
-
-phi_func=@(r,sigma) exp(-(r.^2)/(2*sigma.^2));
+[source.nodes_deform]= pointCloudMorph(target.nodes,source.nodes,params);
 
 
-%% plot original
+%% morph small f
+params.f=10;
+params.new_knots_per_iter=2;
+params.f_decay=.99;
+params.target_source_switch_iter=2;
+params.start_target=0;
+params.max_iterations=10;
+params.include_rand_knots=1;
+params.rand_mult=0.1;
+params.initial_knots=6;
+[source.nodes_deform]= pointCloudMorph(target.nodes,source.nodes_deform,params);
 
 
 
-%% main loop for mesh morphing
-tic
-counter=1;
-error_hist=[];
-switcher=0;
-while counter<=50
-    
-    % get sigmas
-    [~,D] = knnsearch(K,K,'K',4);
-    sigmas=mean(D(:,2:end),2)*f;
-    
-    
-    % determine nearest points
-    [Idx_T] = knnsearch(source_nodes,target_nodes,'K',1);
-    U=target_nodes;
-    d=source_nodes(Idx_T,:);
-    
-    rbf_mat_test=rbfFuncMat(phi_func,d,K,sigmas);
-    
-    % fit rbf A coefficients
-    A=rbf_mat_test\U;
-    
-    % determine new node locations
-    rbf_mat_full=rbfFuncMat(phi_func,source_nodes,K,sigmas);
-    new_source_nodes=rbf_mat_full*A;
-    
-    figure(fig_mesh)
-    clf(fig_mesh)
-    scatter3(source_nodes_0(:,1),source_nodes_0(:,2),source_nodes_0(:,3),'go');
-    hold on
-    scatter3(source_nodes(:,1),source_nodes(:,2),source_nodes(:,3),'bo');
-    scatter3(target_nodes(:,1),target_nodes(:,2),target_nodes(:,3),'ro');
-    scatter3(K(:,1),K(:,2),K(:,3),'kx')
-    axis equal
-    pause(.01);
-    
-    
-    %determine new knot lcoations
-%     if mod(counter,2)==0
-if switcher==0
-        [Idx_n,D_n] = knnsearch(source_nodes,target_nodes,'K',1);
-        temp_dist=[Idx_n,D_n];
-        temp_dist=sortrows(temp_dist,2,'descend');
-        new_indices=unique(temp_dist(:,1),'stable');
-        new_knot_indices=new_indices(1:p);
-        K=[K;source_nodes(new_knot_indices,:)+rand(p,3)*rand_mult];
-        
-    else
-        [Idx_n,D_n] = knnsearch(target_nodes,source_nodes,'K',1);
-        temp_dist=[Idx_n,D_n];
-        temp_dist=sortrows(temp_dist,2,'descend');
-        new_indices=unique(temp_dist(:,1),'stable');
-        new_knot_indices=new_indices(1:p);
-        K=[K;target_nodes(new_knot_indices,:)+rand(p,3)*rand_mult];
-    end
-    
-%     rand_nodes_x=rand(p,1)*bb_dif(1)+bb_min(1);
-%     rand_nodes_y=rand(p,1)*bb_dif(2)+bb_min(2);
-%     rand_nodes_z=rand(p,1)*bb_dif(3)+bb_min(3);
-%     rand_nodes=[rand_nodes_x,rand_nodes_y,rand_nodes_z];
-%     K=[K;rand_nodes];
-    
-    error=max(D_n)
-    error_hist=[error_hist,error];
-    
-    figure(fig_error);
-    plot(error_hist);
-    
-    source_nodes=new_source_nodes;
-    
-    counter=counter+1;
-    
-    rand_mult=rand_mult*1;
-%     f=.95*f;
-    f=.995*f;
-    if mod(counter,10)==0
-%         current_rand_ind=randperm(size(K,1));
-        K=K0;
-%         K=K(current_rand_ind(1:20),:);
-    end
-    
-%     if mod(counter,2)==0
-        if mod(counter,20)==0
-        if switcher==1
-            switcher=0;
-        else
-            switcher=1;
-        end
-    end
-    
-end
-toc
+%% temp
+% % % % % %% mesh morphin initialization
+% % % % % target_nodes=target.nodes;
+% % % % % source_nodes_0=source.nodes_reduce;
+% % % % % source_nodes=source_nodes_0;
+% % % % % bb_min=min(source.nodes_reduce);
+% % % % % bb_max=max(source.nodes_reduce);
+% % % % % bb_dif=abs(bb_max-bb_min)*.3;
+% % % % % bb_min=bb_min-bb_dif;
+% % % % % bb_max=bb_max+bb_dif;
+% % % % % 
+% % % % % x_bb=linspace(bb_min(1),bb_max(1),3);
+% % % % % y_bb=linspace(bb_min(2),bb_max(2),3);
+% % % % % z_bb=linspace(bb_min(3),bb_max(3),3);
+% % % % % 
+% % % % % [X,Y,Z]=meshgrid(x_bb,y_bb,z_bb);
+% % % % % X=reshape(X,[],1);
+% % % % % Y=reshape(Y,[],1);
+% % % % % Z=reshape(Z,[],1);
+% % % % % 
+% % % % % bb_dif=bb_max-bb_min;
+% % % % % K0=[X,Y,Z];
+% % % % % 
+% % % % % K=K0;
+% % % % % % hold on
+% % % % % % scatter3(K(:,1),K(:,2),K(:,3),'go')
+% % % % % 
+% % % % % phi_func=@(r,sigma) exp(-(r.^2)/(2*sigma.^2));
+% % % % % 
+% % % % % 
+% % % % % %% plot original
+% % % % % 
+% % % % % 
+% % % % % 
+% % % % % %% main loop for mesh morphing
+% % % % % tic
+% % % % % counter=1;
+% % % % % error_hist=[];
+% % % % % switcher=0;
+% % % % % while counter<=50
+% % % % %     
+% % % % %     % get sigmas
+% % % % %     [~,D] = knnsearch(K,K,'K',4);
+% % % % %     sigmas=mean(D(:,2:end),2)*f;
+% % % % %     
+% % % % %     
+% % % % %     % determine nearest points
+% % % % %     [Idx_T] = knnsearch(source_nodes,target_nodes,'K',1);
+% % % % %     U=target_nodes;
+% % % % %     d=source_nodes(Idx_T,:);
+% % % % %     
+% % % % %     rbf_mat_test=rbfFuncMat(phi_func,d,K,sigmas);
+% % % % %     
+% % % % %     % fit rbf A coefficients
+% % % % %     A=rbf_mat_test\U;
+% % % % %     
+% % % % %     % determine new node locations
+% % % % %     rbf_mat_full=rbfFuncMat(phi_func,source_nodes,K,sigmas);
+% % % % %     new_source_nodes=rbf_mat_full*A;
+% % % % %     
+% % % % %     figure(fig_mesh)
+% % % % %     clf(fig_mesh)
+% % % % %     scatter3(source_nodes_0(:,1),source_nodes_0(:,2),source_nodes_0(:,3),'go');
+% % % % %     hold on
+% % % % %     scatter3(source_nodes(:,1),source_nodes(:,2),source_nodes(:,3),'bo');
+% % % % %     scatter3(target_nodes(:,1),target_nodes(:,2),target_nodes(:,3),'ro');
+% % % % %     scatter3(K(:,1),K(:,2),K(:,3),'kx')
+% % % % %     axis equal
+% % % % %     pause(.01);
+% % % % %     
+% % % % %     
+% % % % %     %determine new knot lcoations
+% % % % % %     if mod(counter,2)==0
+% % % % % if switcher==0
+% % % % %         [Idx_n,D_n] = knnsearch(source_nodes,target_nodes,'K',1);
+% % % % %         temp_dist=[Idx_n,D_n];
+% % % % %         temp_dist=sortrows(temp_dist,2,'descend');
+% % % % %         new_indices=unique(temp_dist(:,1),'stable');
+% % % % %         new_knot_indices=new_indices(1:p);
+% % % % %         K=[K;source_nodes(new_knot_indices,:)+rand(p,3)*rand_mult];
+% % % % %         
+% % % % %     else
+% % % % %         [Idx_n,D_n] = knnsearch(target_nodes,source_nodes,'K',1);
+% % % % %         temp_dist=[Idx_n,D_n];
+% % % % %         temp_dist=sortrows(temp_dist,2,'descend');
+% % % % %         new_indices=unique(temp_dist(:,1),'stable');
+% % % % %         new_knot_indices=new_indices(1:p);
+% % % % %         K=[K;target_nodes(new_knot_indices,:)+rand(p,3)*rand_mult];
+% % % % %     end
+% % % % %     
+% % % % % %     rand_nodes_x=rand(p,1)*bb_dif(1)+bb_min(1);
+% % % % % %     rand_nodes_y=rand(p,1)*bb_dif(2)+bb_min(2);
+% % % % % %     rand_nodes_z=rand(p,1)*bb_dif(3)+bb_min(3);
+% % % % % %     rand_nodes=[rand_nodes_x,rand_nodes_y,rand_nodes_z];
+% % % % % %     K=[K;rand_nodes];
+% % % % %     
+% % % % %     error=max(D_n)
+% % % % %     error_hist=[error_hist,error];
+% % % % %     
+% % % % %     figure(fig_error);
+% % % % %     plot(error_hist);
+% % % % %     
+% % % % %     source_nodes=new_source_nodes;
+% % % % %     
+% % % % %     counter=counter+1;
+% % % % %     
+% % % % %     rand_mult=rand_mult*1;
+% % % % % %     f=.95*f;
+% % % % %     f=.995*f;
+% % % % %     if mod(counter,10)==0
+% % % % % %         current_rand_ind=randperm(size(K,1));
+% % % % %         K=K0;
+% % % % % %         K=K(current_rand_ind(1:20),:);
+% % % % %     end
+% % % % %     
+% % % % % %     if mod(counter,2)==0
+% % % % %         if mod(counter,20)==0
+% % % % %         if switcher==1
+% % % % %             switcher=0;
+% % % % %         else
+% % % % %             switcher=1;
+% % % % %         end
+% % % % %     end
+% % % % %     
+% % % % % end
+% % % % % toc
 
 %% plot final meshes
 figure()
 target_geom_orig=patch('Faces',target.faces,'Vertices',target.nodes,'FaceColor','r','EdgeAlpha',.2);
 hold on
-source_geom_orig=patch('Faces',source.faces_reduce,'Vertices',source_nodes,'FaceColor','b','EdgeAlpha',.2);
-source_geom_fin=patch('Faces',source.faces_reduce,'Vertices',source.nodes_reduce,'FaceColor','g','EdgeAlpha',.2);
+source_geom_orig=patch('Faces',source.faces,'Vertices',source.nodes_deform,'FaceColor','b','EdgeAlpha',.2);
+source_geom_fin=patch('Faces',source.faces,'Vertices',source.nodes,'FaceColor','g','EdgeAlpha',.2);
 axis equal
 
 
 %% get net change
-source.change=source_nodes-source.nodes;
-deform_net=newrbe(source.nodes',source.change',100);
+source.change=source.nodes_deform-source.nodes;
+deform_net=newgrnn(source.nodes',source.change');
 
 %% get new points
-source.nodes_deform=[sim(deform_net,source.nodes_orig(:,2:end)')]'+source.nodes_orig(:,2:end);
+source.nodes_deform_total=[sim(deform_net,source.nodes_orig(:,2:end)')]'+source.nodes_orig(:,2:end);
 
 
 %% plot points
@@ -218,8 +230,18 @@ figure()
 target_geom_orig=patch('Faces',target.faces,'Vertices',target.nodes,'FaceColor','r','EdgeAlpha',.2);
 
 hold on
-source_geom_deform=patch('Faces',source_faces_plot,'Vertices',source.nodes_deform,'FaceColor','m');
+source_geom_deform=patch('Faces',source_faces_plot,'Vertices',source.nodes_deform_total,'FaceColor','m');
 axis equal
+
+%% deform inner nodes
+
+
+
+
+
+
+
+
 %% coherent point drift method
 % [registered]=nonrigidICPv1(target.nodes,source_nodes,target.faces,source.faces_reduce,50,1);
 
---
+++ pointCloudMorph.m
@@ -0,0 +1,138 @@
+function [source_nodes_fit]= pointCloudMorph(target_nodes,source_nodes,params)
+    f=params.f;
+    f_decay=params.f_decay;
+    max_iterations=params.max_iterations;
+    want_plot=params.want_plot;
+    rand_mult=params.rand_mult;
+    rand_decay=params.rand_decay;
+    include_rand_knots=params.include_rand_knots;
+    knot_reset_iter=params.knot_reset_iter;
+    target_source_switch_iter=params.target_source_switch_iter;
+    start_target=params.start_target;
+    p=params.new_knots_per_iter;
+    initial_knots=params.initial_knots;
+    
+    %% mesh morphin initialization
+    source_nodes_0=source_nodes;
+    bb_min=min(source_nodes_0);
+    bb_max=max(source_nodes_0);
+    bb_dif=abs(bb_max-bb_min)*.3;
+    bb_min=bb_min-bb_dif;
+    bb_max=bb_max+bb_dif;
+
+    x_bb=linspace(bb_min(1),bb_max(1),initial_knots);
+    y_bb=linspace(bb_min(2),bb_max(2),initial_knots);
+    z_bb=linspace(bb_min(3),bb_max(3),initial_knots);
+
+    [X,Y,Z]=meshgrid(x_bb,y_bb,z_bb);
+    X=reshape(X,[],1);
+    Y=reshape(Y,[],1);
+    Z=reshape(Z,[],1);
+
+    bb_dif=bb_max-bb_min;
+    K0=[X,Y,Z];
+
+    K=K0;
+    phi_func=@(r,sigma) exp(-(r.^2)/(2*sigma.^2));
+
+    %% main loop for mesh morphing
+    counter=1;
+    error_hist=[];
+    switcher=start_target;
+    while counter<=max_iterations
+        % get sigmas
+        [~,D] = knnsearch(K,K,'K',4);
+        sigmas=mean(D(:,2:end),2)*f;
+
+        % determine nearest points
+        [Idx_T] = knnsearch(source_nodes,target_nodes,'K',1);
+        U=target_nodes;
+        d=source_nodes(Idx_T,:);
+        rbf_mat_test=rbfFuncMat(phi_func,d,K,sigmas);
+
+        % fit rbf A coefficients
+        A=rbf_mat_test\U;
+
+        % determine new node locations
+        rbf_mat_full=rbfFuncMat(phi_func,source_nodes,K,sigmas);
+        new_source_nodes=rbf_mat_full*A;
+
+        if want_plot==1
+            if counter==1
+                fig_mesh=figure();
+            else
+                fig_mesh;
+            end
+            clf(fig_mesh);
+            subplot(1,2,1);
+            scatter3(source_nodes_0(:,1),source_nodes_0(:,2),source_nodes_0(:,3),'go');
+            hold on
+            scatter3(source_nodes(:,1),source_nodes(:,2),source_nodes(:,3),'bo');
+            scatter3(target_nodes(:,1),target_nodes(:,2),target_nodes(:,3),'ro');
+            scatter3(K(:,1),K(:,2),K(:,3),'kx')
+            axis equal
+            pause(.01);
+        end
+
+
+        %determine new knot lcoations
+        %     if mod(counter,2)==0
+        if switcher==0
+            [Idx_n,D_n] = knnsearch(source_nodes,target_nodes,'K',1);
+            temp_dist=[Idx_n,D_n];
+            temp_dist=sortrows(temp_dist,2,'descend');
+            new_indices=unique(temp_dist(:,1),'stable');
+            new_knot_indices=new_indices(1:p);
+            K=[K;source_nodes(new_knot_indices,:)+rand(p,3)*rand_mult];
+
+        else
+            [Idx_n,D_n] = knnsearch(target_nodes,source_nodes,'K',1);
+            temp_dist=[Idx_n,D_n];
+            temp_dist=sortrows(temp_dist,2,'descend');
+            new_indices=unique(temp_dist(:,1),'stable');
+            new_knot_indices=new_indices(1:p);
+            K=[K;target_nodes(new_knot_indices,:)+rand(p,3)*rand_mult];
+        end
+
+        if include_rand_knots==1
+        
+            rand_nodes_x=rand(p,1)*bb_dif(1)+bb_min(1);
+            rand_nodes_y=rand(p,1)*bb_dif(2)+bb_min(2);
+            rand_nodes_z=rand(p,1)*bb_dif(3)+bb_min(3);
+            rand_nodes=[rand_nodes_x,rand_nodes_y,rand_nodes_z];
+            K=[K;rand_nodes];
+        end
+
+        
+        error=max(D_n);
+        if want_plot==1
+            subplot(1,2,2);
+            error_hist=[error_hist,error];
+            plot(error_hist);
+            pause(.05);
+        end
+
+        
+        source_nodes=new_source_nodes;
+        counter=counter+1;
+
+        rand_mult=rand_mult*rand_decay;
+        f=f_decay*f;
+        
+        if mod(counter,knot_reset_iter)==0
+            K=K0;
+        end
+
+        %     if mod(counter,2)==0
+        if mod(counter,target_source_switch_iter)==0
+            if switcher==1
+                switcher=0;
+            else
+                switcher=1;
+            end
+        end
+
+    end
+    source_nodes_fit=source_nodes;
+
+end
